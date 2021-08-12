fa54c574e24d3c24d34372e6054b4bfafe20ef45
Andrew Andreev <aandreev@ivi.ru>
2021-08-12 14:08:43 +0300

    Создать структуру Rule

---
+++ internal/models/rule_test.go
@@ -0,0 +1,15 @@
+package models
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRuleMethods(t *testing.T) {
+	rule := Rule{Name: "Number One", UserID: 123}
+	assert.Equal(t, "<Number One: Count = 0>", rule.String())
+
+	rule.IncCount()
+	assert.Equal(t, "<Number One: Count = 1>", rule.String())
+}
