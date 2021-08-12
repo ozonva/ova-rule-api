69637e931a8659b63de14a53a02a5940c68eaef1
Andrew Andreev <aandreev@ivi.ru>
2021-08-12 13:43:12 +0300

    Реализовать чтение конфигов

---
+++ configs/configs_test.go
@@ -0,0 +1,25 @@
+package configs
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConfigsLoad(t *testing.T) {
+	serverCfg := Server{
+		Host: "localhost",
+		Port: 8000,
+	}
+	databaseCfg := Database{
+		Host:     "localhost",
+		Port:     5432,
+		Username: "ova",
+		Password: "",
+	}
+
+	Load()
+
+	assert.Equal(t, serverCfg, *ServerConfig)
+	assert.Equal(t, databaseCfg, *DatabaseConfig)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/ozonva/ova-rule-api
 
 go 1.16
+
+require (
+	github.com/stretchr/testify v1.7.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
