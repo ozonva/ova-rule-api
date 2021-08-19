b814b4c667de1a1ec18c8d4215c261c6b98f462d
Andrew Andreev <aandreev@ivi.ru>
2021-08-19 10:42:18 +0300

    Реализовать имплементацию flusher для интерфейса Flusher

--- internal/flusher/flusher.go
+++ internal/flusher/flusher.go
@@ -3,6 +3,7 @@ package flusher
 import (
 	"github.com/ozonva/ova-rule-api/internal/models"
 	"github.com/ozonva/ova-rule-api/internal/repo"
+	"github.com/ozonva/ova-rule-api/internal/utils"
 )
 
 // Flusher - интерфейс для сброса задач в хранилище.
@@ -28,6 +29,18 @@ type flusher struct {
 
 // Flush сбрасывает задачи в хранилище.
 func (f *flusher) Flush(rules []models.Rule) []models.Rule {
-	// TODO: имплементировать логику
-	return rules
+	var result []models.Rule
+
+	chunks, err := utils.SplitToBulks(rules, f.chunkSize)
+	if err != nil {
+		return rules
+	}
+
+	for i, chunk := range chunks {
+		if err = f.ruleRepo.AddRules(chunk); err != nil {
+			result = append(result, chunks[i]...)
+		}
+	}
+
+	return result
 }
